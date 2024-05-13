1bb0d59f2d7e45196e25cbe38ab65655cde5b91a
Luis <luisriveraphi@gmail.com>
2024-05-13 00:48:40 -0400

    Added code to the components and pages files

---
+++ MyReactPortfolio/src/pages/About.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About me</h1>
+      <p>
+        Hello, I'm Luis! A new but inspired web developer looking to continously learn and grow within the tech industry. I've always had an interest for coding, but never took the leap of faith, until now! Through my journey i'm excited to pick up new concepts, overcome challenges, build friendships, and develop incredible applications! I really enjoy making projects work dynamically aswell as applying awesome styles to really bring tie it together and bring them into life! Below you can see what I've created so far.
+      </p>
+    </div>
+  )
+}
